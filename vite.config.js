ca29ccdd2a3af5e05847754419435fed446efcac
adarshn-dev <adarshaadhu580@gmail.com>
2025-05-30 15:59:16 +0530

    done

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/ADNX-SHOP/", // Set this to your repo name
+  base: "/", // Use root for custom domain
   plugins: [react()],
   build: {
     outDir: "docs",
